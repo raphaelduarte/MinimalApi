55fd8fea68b499fec9beb415bdb708511ac13786
Raphael Duarte <raphaelduartessph@gmail.com>
2023-05-27 01:05:58 -0300

    Add project files.

---
+++ MinimalApi/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
